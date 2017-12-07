471262274086683bbf91b172fd41da82d763e398
qianbajin <qianbajin@gmail.com>
2017-12-07 18:34:13 +0800

    修改主页

--- app/src/main/java/com/qianbajin/sportaccelerator/MainActivity.java
+++ app/src/main/java/com/qianbajin/sportaccelerator/MainActivity.java
@@ -25,123 +25,29 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+  
+    private static final String SP_PATH = "chmod 755 /data/data/com.qianbajin.sportaccelerator/shared_prefs/com.qianbajin.sportaccelerator_preferences.xml";
+    private static final String SP_PATH_P = "chmod 755 /data/data/com.qianbajin.sportaccelerator/shared_prefs";
+    private static final String SP_PATH_pp = "chmod 755 /data/data/com.qianbajin.sportaccelerator";
+    
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        getFragmentManager().beginTransaction().replace(R.id.fl_content, new SettingFragment()).commit();
 
-        TabLayout tabLayout = findViewById(R.id.tab);
-        ViewPager viewPager = findViewById(R.id.vp_content);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_content);
         tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.app)));
         tabLayout.addTab(tabLayout.newTab().setText(getString(R.string.setting)));
-//        tabLayout.setupWithViewPager(viewPager);
-
 
         SparseArrayCompat<Fragment> arrayCompat = new SparseArrayCompat<>(2);
         arrayCompat.put(0, new AppListFragment());
         arrayCompat.put(1, new SettingFragment());
         String[] strings = getResources().getStringArray(R.array.adapt_title);
 
-        viewPager.setAdapter(new PaperAdapter(getSupportFragmentManager(),strings, arrayCompat));
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-//                getSupportFragmentManager().beginTransaction().add(R.id.fl_content, new SettingFragment()).commit();
-
-                int a = 0x2;
-                Log.d("MainActivity", "a:" + a);
-                PackageManager pm = getPackageManager();
-
-                long millis = System.currentTimeMillis();
-//                SimpleDateFormat format6 = new SimpleDateFormat("yyyyMMdd_HH:mm:ss");
-                DateFormat format6 = DateFormat.getDateTimeInstance();
-
-                String format = format6.format(new Date(1511671883040L));
-                Log.d("MainActivity", "format:" + format);
-                String format0 = format6.format(new Date(1511653789541L));
-
-                Log.d("MainActivity", "format0:" + format0);
-                String format1 = format6.format(new Date(1511691064473L));
-                Log.d("MainActivity", "format1:" + format1);
-                String format2 = format6.format(new Date(1511653789541L));
-                Log.d("MainActivity", "format2:" + format2);
-
-                String format3 = format6.format(new Date(1511701214563L));
-                Log.d("MainActivity", "format3:" + format3);
-
-                String format4 = format6.format(new Date(1511701224352L));
-                Log.d("MainActivity", "format4:" + format4);
-
-                String format5 = format6.format(new Date(1511671883040L));
-                Log.d("MainActivity", "format5:" + format5);
-
-                long l = System.currentTimeMillis() - millis;
-                Log.d("MainActivity", "l:" + l);
-
-//                try {
-//                    ApplicationInfo applicationInfo = pm.getApplicationInfo(SportHook.ALIPAY, PackageManager.GET_META_DATA);
-//                    int i = applicationInfo.describeContents();
-//                    Drawable drawable = applicationInfo.loadIcon(pm);
-//                    Log.d("MainActivity", "drawable:" + drawable);
-//                    int descriptionRes = applicationInfo.descriptionRes;
-//                    Log.d("MainActivity", "i:" + i);
-//                    Log.d("MainActivity", "descriptionRes:" + descriptionRes);
-//                } catch (PackageManager.NameNotFoundException e) {
-//                    e.printStackTrace();
-//                }
-//                List<PackageInfo> infoList = pm.getInstalledPackages(PackageManager.GET_META_DATA);
-//                for (PackageInfo packageInfo : infoList) {
-//                    ApplicationInfo applicationInfo = packageInfo.applicationInfo;
-//                    int flags = applicationInfo.flags;
-//                    Drawable drawable = applicationInfo.loadIcon(pm);
-//                    boolean enabled = applicationInfo.enabled;
-//                    Log.d("MainActivity", "enabled:" + enabled);
-//                    Log.d("MainActivity", "applicationInfo:" + applicationInfo.packageName);
-//
-//                }
-
-                Snackbar.make(view, /*"Replace with your own action" +*/ getTopActivityName(), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_PACKAGE_ADDED);
-        intentFilter.addAction(Intent.ACTION_PACKAGE_CHANGED);
-        intentFilter.addAction(Intent.ACTION_PACKAGE_RESTARTED);
-        intentFilter.addAction(Intent.ACTION_PACKAGE_REPLACED);
-        intentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED);
-        registerReceiver(new PackageAction(), intentFilter);
+        viewPager.setAdapter(new PaperAdapter(getSupportFragmentManager(), strings, arrayCompat));
 
     }
-
-    public String getTopActivityName() {
-        String processName = null;
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-//        List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = manager.getRunningAppProcesses();
-//        if (runningAppProcesses != null && !runningAppProcesses.isEmpty()) {
-//            ActivityManager.RunningAppProcessInfo runningAppProcessInfo = runningAppProcesses.get(0);
-//            runningAppProcessInfo.
-//                    processName = runningAppProcesses.get(0).processName;
-//        }
-        List<ActivityManager.RunningTaskInfo> runningTasks = manager.getRunningTasks(1);
-        String className = runningTasks.get(0).topActivity.getClassName();
-
-        return className;
-    }
-
-    private static class PackageAction extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            Log.d("PackageAction", "action:" + action);
-        }
-    }
-
 
     static class PaperAdapter extends FragmentStatePagerAdapter {
 
@@ -168,6 +74,25 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mTitle[position];
         }
+    }
+
+
+    private boolean chmod() {
+        try {
+            Runtime.getRuntime().exec(SP_PATH_pp);
+            Runtime.getRuntime().exec(SP_PATH_P);
+            Runtime.getRuntime().exec(SP_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        chmod();
+        super.onBackPressed();
     }
 
 }
