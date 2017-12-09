9037e236f76de56dea8f1d8fc25eeb2ecc72ea3a
qianbajin <qianbajin@gmail.com>
2017-12-09 20:32:47 +0800

    AliFragment

--- app/src/main/java/com/qianbajin/sportaccelerator/fragment/AliFragment.java
+++ app/src/main/java/com/qianbajin/sportaccelerator/fragment/AliFragment.java
@@ -1,10 +1,5 @@
 package com.qianbajin.sportaccelerator.fragment;
 
-import android.preference.ListPreference;
-import android.text.TextUtils;
-import android.util.Log;
-
-import com.qianbajin.sportaccelerator.Constant;
 import com.qianbajin.sportaccelerator.R;
 /**
  * @author Administrator
@@ -17,14 +12,5 @@ public class AliFragment extends BasePreferenceFragment {
     @Override
     protected int getXmlId() {
         return R.xml.config_alipay;
-    }
-
-    @Override
-    protected void init() {
-        super.init();
-        ListPreference preference = (ListPreference) findPreference(Constant.PK_ALIPAY);
-        CharSequence entry = preference.getEntry();
-        Log.d(TAG, "entry:" + entry);
-        preference.setSummary(TextUtils.isEmpty(entry) ? getString(R.string.edit) : entry);
     }
 }
