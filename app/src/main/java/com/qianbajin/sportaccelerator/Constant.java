585599159d090457879c5ad40996458fdcf8a505
qianbajin <qianbajin@gmail.com>
2017-12-09 19:34:14 +0800

    常量

--- app/src/main/java/com/qianbajin/sportaccelerator/Constant.java
+++ app/src/main/java/com/qianbajin/sportaccelerator/Constant.java
@@ -16,18 +16,19 @@ public class Constant {
     public static final String PK_ALIPAY = "com.eg.android.AlipayGphone";
     public static final String ALI_EXT = "com.eg.android.AlipayGphone:ext";
     public static final String PK_QQ = "com.tencent.mobileqq";
-    public static int ALI_STEP_TOTAL;
+    public static final String QQ_MSF = "com.tencent.mobileqq:MSF";
     public static final String SP_KEY_ALI_UPPER_LIMIT = "sp_key_ali_upper_limit";
     public static final String SP_KEY_QQ_UPPER_LIMIT = "sp_key_qq_upper_limit";
     public static final String SP_KEY_ALI_STEP_EDIT = "sp_key_ali_step_edit";
     public static final String SP_KEY_ALI_RATE = "sp_key_ali_rate";
-    public static final String SP_KEY_ALI_EXPECT_STEP = "sp_key_ali_expect_step";
+    public static final String SP_KEY_ALI_GAIN_STEP = "sp_key_ali_gain_step";
     public static final String ALI_SP_KEY_BASESTEP = "baseStep";
     public static final String ALI_SP_KEY_STEPRECORD = "stepRecord";
     public static final String SP_KEY_QQ_RATE = "sp_key_qq_rate";
     public static final String SP_KEY_SENSOR_LOG = "sp_key_sensor_log";
     public static final String SP_KEY_CONFIG_LOG = "sp_key_config_log";
-    public static final String SP_KEY_DELETE_ALI_SPORT_RECORD = "sp_key_delete_ali_sport_record";
-
+    public static final String SP_KEY_ALI_SENSOR = "sensor";
+    public static final String SP_KEY_ALI_EDIT = "edit";
+    public static int ALI_STEP_TOTAL;
 
 }
